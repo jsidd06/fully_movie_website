a01ad0ec7240d375e72fc014963dab386c5922aa
siddharth <jsidd06@gmail.com>
2022-03-07 11:11:55 +0530

    using search api wth async wait function and fetch and call the api in search Box using dynamic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import MovieList from './Components/Movie/MovieList/MovieList';
@@ -8,11 +8,30 @@ import SearchBox from './Components/SearchBox/SearchBox';
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchMovieValue, setSearchMovieValue] = useState("");
+
+
+  const getMoviesResult = async (searchMovieValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchMovieValue}&apikey=e5878fb5`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if(responseJson.Search){
+      setMovies(responseJson.Search);
+    }
+  }
+
+  useEffect(() => {
+    getMoviesResult(searchMovieValue);
+  } , [searchMovieValue]);
+
+
+
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
         <MovieListHeading heading="Movies" />
-        <SearchBox searchValue={searchMovieValue} setSearchMovieValue={searchMovieValue} />
+        <SearchBox searchValue={searchMovieValue} setSearchMovieValue={setSearchMovieValue} />
       </div>
       <div className="row">
         <MovieList movies={movies} />
