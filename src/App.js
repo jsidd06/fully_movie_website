c2520fa507e1f8029af432c5730c0129de5ffd96
siddharth <jsidd06@gmail.com>
2022-03-07 14:15:49 +0530

    add and remove the data in localstorage

--- src/App.js
+++ src/App.js
@@ -28,14 +28,24 @@ function App() {
     getMoviesResult(searchMovieValue);
   } , [searchMovieValue]);
 // we are editing the state of the favorite array here and we are adding the movie to the array so that we can display it in the favorite list and also save the data using spread operator in local storage
+
+const saveToLocalStorage = (items) => {
+  localStorage.setItem('react-movie-app-favorite', JSON.stringify(items));
+}
+
+
 const AddFavoriteMovie = (movie) => {
   const newFavoriteList = [...favorite, movie];
+  // save data in local   storage 
   setFavorite(newFavoriteList);
+  saveToLocalStorage(newFavoriteList);
 }
 
 const RemoveFavoriteMovie = (movie) => {
   const newFavoriteList = favorite.filter(item => item.imdbID !== movie.imdbID);
+  // remove data list  local storage
   setFavorite(newFavoriteList);
+  saveToLocalStorage(newFavoriteList);
 }
 
   return (
